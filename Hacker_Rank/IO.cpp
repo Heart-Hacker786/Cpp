19a254653c5e6dc6fbba018b1d4ed0a4ad766d28
Heart-Hacker786 <bcsf19a513@pucit.edu.pk>
2020-10-24 23:22:24 +0500

    added hacker_rank Practices

---
+++ Hacker_Rank/IO.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int num[3];
+    cin>>num[0]>>num[1]>>num[2];
+    cout<<  num[0]+num[1]+num[2];
+    return 0;
+}
+
