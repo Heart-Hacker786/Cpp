00a9eb8a56e9956088dd6c869d781489e6f0eb4d
Heart-hacker786 <abdul.wahab.tanveer009@gmail.com>
2021-03-06 23:30:43 +0500

    LAB_02

--- Semester_04/DSA_LABs/LAB_02_6_MAR_21/Graph_Search.cpp
+++ Semester_04/DSA_LABs/LAB_02_6_MAR_21/Graph_Search.cpp
@@ -5,15 +5,15 @@ using namespace std;
 int main(){
     
     int n;int center,oper,n1=0,n2=0;
-    cin>>n;
+    cin>>n;//5
     if(n%2==0)
         return 0;
     else center=(n/2)+1;
 
 int ptr[n][n];
 
-    int rangep=n-center;
-    int rangen=-n+center;
+    int rangep=n-center;//allowed values 2
+    int rangen=-n+center;//negative
   
     
     for(int i=0;i<n;i++)
@@ -26,11 +26,11 @@ int ptr[n][n];
             cin>>n2;
            if(n1>rangep||n1<rangen||n2>rangep||n2<rangen){
             throw 2;}
+            
         n1=n1+center-1;
-        n2=(-1*n2)+center-1;
+        n2=(-n2)+center-1;
         
-        
-         cout<<ptr[n2][n1]<<endl;}
+        cout<<ptr[n2][n1]<<endl;}
         catch(const int i){
         cout<<"INVALID INPUT"<<endl;
         }
